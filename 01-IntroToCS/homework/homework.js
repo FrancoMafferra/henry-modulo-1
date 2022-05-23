174dc6ea35b19b6e2e6b4d9d41eb87e0e8f6c768
Franco Mafferra <francomafferra38@gmail.com>
2022-05-23 15:28:03 -0300

    finsh homework introducción

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,13 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
-}
+  var numArr = num.split("");
+  var suma = 0;
+  for(var i = 0; i < numArr.length; i++){
+    suma = suma + Math.pow(2, numArr.length-i -1) * numArr[i];
+  };
+  return suma;
+};
 
 function DecimalABinario(num) {
   // tu codigo aca
-
-}
+  let number = num;
+  let binary = (number % 2).toString();
+  for (; number > 1; ) {
+      number = parseInt(number / 2);
+      binary =  (number % 2) + (binary);
+  };
+  return binary; 
+};
 
 
 module.exports = {
